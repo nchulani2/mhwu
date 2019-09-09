4f77db0ba9827becf2a8802923f28d552156fd99
Naveen Chulani <nchulani2@gmail.com>
2019-09-09 11:45:14 -0500

    update

--- src/Reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import monstersReducer from './monstersReducer';
-import skillsReducer from './skillsReducer';
-
-export default combineReducers({
-  data: monstersReducer,
-  skills: skillsReducer
-});
